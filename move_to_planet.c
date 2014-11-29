9221307d8412f072036cbb3b083cddff26595e32
Davis Yarbrough <dyarbrough93@yahoo.com>
2014-11-28 19:04:44 -0600

    Update move_to_planet.c

--- move_to_planet.c
+++ move_to_planet.c
@@ -1,5 +1,6 @@
 void move_to_planet(int i)
 {
+  	// move_to_planet:
   	// take off
 	int p_x = planets[i].x, p_y = planets[i].y, p_rad = planets[i].radius;
 	int bot_x = bot.x, bot_y = bot.y;
@@ -7,13 +8,14 @@ void move_to_planet(int i)
 	bot.x_vel = bot_x < p_x ? 10 : -10;
 
 	while (abs(bot.x - p_x) > p_rad - 3); // spin spin
-
+	// mtp_move_y:
 	bot.x_vel = 0;
 	bot.y_vel = bot_y < p_y ? 10 : -10;
 
 	while (abs(bot.y - p_y) > p_rad - 3);
-
+	// mtp_land:
 	bot.y_vel = 0;
 
 	while (LANDING_REQUEST == -1);
+	// mtp_ret:
 }
