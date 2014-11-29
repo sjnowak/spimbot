684599435773c0c80571db4338619824c2db3838
Davis Yarbrough <dyarbrough93@yahoo.com>
2014-11-28 19:20:59 -0600

    Update move_to_planet.c

--- move_to_planet.c
+++ move_to_planet.c
@@ -7,33 +7,33 @@ void move_to_planet(int i)
 
 	if (bot_x < p_x) {
 		bot.angle = 0;
-		bot.x_vel = 10;
+		bot.vel = 10;
 	}
 	// x_check_else:
 	else {
 		bot.angle = 180;
-		bot.x_vel = -10;
+		bot.vel = -10;
 	}
 
 	// mtp_move_x_loop:
 	while (abs(bot.x - p_x) > p_rad - 3); // spin spin
 	// y_check:
-	bot.x_vel = 0;
+	bot.vel = 0;
 	
 	if (bot_y < p_y) {
 		bot.angle = 90;
-		bot.y_vel = 10;
+		bot.vel = 10;
 	}
 	// y_check_else:
 	else {
 		bot.angle = 270;
-		bot.y_vel = -10;
+		bot.vel = -10;
 	}
 		
 	// mtp_move_y_loop:
 	while (abs(bot.y - p_y) > p_rad - 3);
 	// mtp_land:
-	bot.y_vel = 0;
+	bot.vel = 0;
 
 	while (LANDING_REQUEST == -1);
 	// mtp_ret:
