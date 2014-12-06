e5cb8bc58ba4ddc2b9dcfdc58e4039d39f8bd760
Davis Yarbrough <dyarbrough93@yahoo.com>
2014-12-06 16:05:12 -0600

    Update spimbot.s

--- spimbot.s
+++ spimbot.s
@@ -1,5 +1,4 @@
-# syscall constants 
-PRINT_STRING_SYS = 4
+# syscall constants (none yet, add as needed)
 
 # movement memory-mapped I/O
 VELOCITY             = 0xffff0010
@@ -53,258 +52,6 @@ puzzles: .space 40960                # 8192 * 5 --- array of puzzles
 
 .text
 
-# puzzle_solver ##################################################
-# 
-# argument $a0: array index of the planet to move to
-# returns       nothing
-puzzle_solver:
-	sub	$sp, $sp, 20
-	sw	$ra, 0($sp)		# save $ra and free up 4 $s registers for
-	sw	$s0, 4($sp)		# str1
-	sw	$s1, 8($sp)		# str2
-	sw	$s2, 12($sp)		# length
-	sw	$s3, 16($sp)		# i
-
-	move	$s0, $a0		# str1
-	move	$s1, $a1		# str2
-
-	jal	my_strlen
-
-	move 	$s2, $v0		# length
-	li	$s3, 0			# i = 0
-ps_loop:
-	bgt	$s3, $s2, ps_return_minus_1
-	move	$a0, $s0		# str1
-	move	$a1, $s1		# str2
-	jal	my_strcmp
-	beq	$v0, $0, ps_return_i
-	
-	move	$a0, $s1		# str2
-	jal	rotate_string_in_place_fast
-	add	$s3, $s3, 1		# i ++
-	j	ps_loop
-
-ps_return_minus_1:
-	li	$v0, -1
-	j	ps_done
-
-ps_return_i:
-	move	$v0, $s3
-
-ps_done:	
-	lw	$ra, 0($sp)		# restore registers and return
-	lw	$s0, 4($sp)
-	lw	$s1, 8($sp)
-	lw	$s2, 12($sp)
-	lw	$s3, 16($sp)
-	add	$sp, $sp, 20
-	jr	$ra
-
-my_strcmp:
-	li	$t3, 0			# i = 0
-my_strcmp_loop:
-	add	$t0, $a0, $t3		# &str1[i]
-	lb	$t0, 0($t0)		# c1 = str1[i]
-	add	$t1, $a1, $t3		# &str2[i]
-	lb	$t1, 0($t1)		# c2 = str2[i]
-
-	beq	$t0, $t1, my_strcmp_equal
-	sub	$v0, $t0, $t1		# c1 - c2
-	jr	$ra
-
-my_strcmp_equal:
-	bne	$t0, $0, my_strcmp_not_done
-	li	$v0, 0
-	jr	$ra
-
-my_strcmp_not_done:
-	add	$t3, $t3, 1		# i ++
-	j	my_strcmp_loop
-
-rotate_string_in_place_fast:
-	sub	$sp, $sp, 8
-	sw	$ra, 0($sp)
-	sw	$a0, 4($sp)
-
-	jal	my_strlen
-	move	$t0, $v0		# length
-	lw	$a0, 4($sp)
-	lb	$t1, 0($a0)		# was_first = str[0]
-
-	div	$t3, $t0, 4		# length_in_ints = length / 4;
-
-	li	$t2, 0			# i = 0
-	move	$a1, $a0		# making copy of 'str' for use in first loop
-rsipf_loop1:
-	bge	$t2, $t3, rsipf_loop2_prologue
-	lw	$t4, 0($a1)		# unsigned first_word = str_as_array_of_ints[i]
-	lw	$t5, 4($a1)		# unsigned second_word = str_as_array_of_ints[i+1]
-	srl	$t6, $t4, 8		# (first_word >> 8)
-	sll	$t7, $t5, 24		# (second_word << 24)
-	or	$t7, $t7, $t6		# combined_word = (first_word >> 8) | (second_word << 24)
-	sw	$t7, 0($a1)		# str_as_array_of_ints[i] = combined_word
-	add	$t2, $t2, 1		# i ++
-	add	$a1, $a1, 4		# str_as_array_of_inst ++
-	j	rsipf_loop1		
-
-rsipf_loop2_prologue:
-	mul	$t2, $t3, 4
-	add	$t2, $t2, 1		# i = length_in_ints*4 + 1
-rsipf_loop2:
-	bge	$t2, $t0, rsipf_done2
-	add	$t3, $a0, $t2		# &str[i]
-	lb	$t4, 0($t3)		# char c = str[i]
-	sb	$t4, -1($t3)		# str[i - 1] = c
-	add	$t2, $t2, 1		# i ++
-	j	rsipf_loop2		
-	
-rsipf_done2:
-	add	$t3, $a0, $t0		# &str[length]
-	sb	$t1, -1($t3)		# str[length - 1] = was_first
-	lw	$ra, 0($sp)
-	add	$sp, $sp, 8
-	jr	$ra
-
-my_strlen:
-	li	$v0, 0			# length = 0  (in $v0 'cause return val)
-my_strlen_loop:
-	add	$t1, $a0, $v0		# &str[length]
-	lb	$t2, 0($t1)		# str[length]
-	beq	$t2, $0, my_strlen_done
-	
-	add	$v0, $v0, 1		# length ++
-	j 	my_strlen_loop
-
-my_strlen_done:
-	jr	$ra
-
-# move_to_planet ##################################################
-# 
-# argument $a0: array index of the planet to move to
-# returns       nothing
-
-move_to_planet:
-
-	sub $sp, $sp, 4
-	sw  $ra, 0($sp)
-
-	sw  $0,  TAKEOFF_REQUEST      # take off
-
-	la  $t0, planets
-	sw  $t0, PLANETS_REQUEST      # t0 = &planets[0]
-
-	li  $t1, 24
-	mul $t1, $t1, $a0
-
-	add $t0, $t0, $t1             # t0 = &planets[i]
-	lw  $t1, planet_x($t0)
-	lw  $t2, planet_y($t0)
-	lw  $t3, planet_radius($t0)
-	sub $t3, $t3, 3               # p_rad - 3
-	lw  $t4, BOT_X
-
-	li  $t5, 1
-	li  $t6, 10
-
-	bge $t4, $t1, mtp_x_check_else
-	sw  $0,  ANGLE
-	sw  $t5, ANGLE_CONTROL
-	j   x_check_done
-
-mtp_x_check_else:
-
-	li  $t7, 180
-	sw  $t7, ANGLE
-	sw  $t5, ANGLE_CONTROL
-
-mtp_x_check_done:
-
-	sw  $t6, VELOCITY
-
-mtp_move_x_loop:
-
-	sub $a0, $t4, $t1
-	abs $v0, $a0
-	ble $v0, $t3, mtp_move_x_done
-	lw  $t4, BOT_X
-	j mtp_move_x_loop
-
-mtp_move_x_done:
-
-	sw  $0, VELOCITY
-
-	lw  $t4, BOT_Y
-	bge $t4, $t2, mtp_y_check_else
-	li  $t7, 90
-	sw  $t7, ANGLE
-	sw  $t5, ANGLE_CONTROL	
-	j   mtp_y_check_done
-
-mtp_y_check_else:
-
-	li  $t7, 270
-	sw  $t7, ANGLE
-	sw  $t5, ANGLE_CONTROL
-
-mtp_y_check_done:
-
-	sw  $t6, VELOCITY
-
-mtp_move_y_loop:
-
-	sub $a0, $t4, $t2
-	abs $v0, $a0
-	ble $v0, $t3, mtp_move_y_done
-	lw  $t4, BOT_Y
-	j   mtp_move_y_loop
-
-mtp_move_y_done:
-	
-	sw  $0, VELOCITY
-
-mtp_land_loop:
-
-	sw  $0, LANDING_REQUEST
-	lw  $t0, LANDING_REQUEST
-	bge $t0, $0, mtp_ret
-	j mtp_land_loop
-
-mtp_ret:
-
-	lw  $ra, 0($sp)
-	add $sp, $sp, 4
-
-	jr  $ra
-
-#  solve_puzzles ###################################################
-#
-# arguments: a0: index
-# return: nothing
-solve_puzzles: 
-
-	sub $sp $sp, 4
-	sw  $ra, 0($sp)
-
-	la  $t0, puzzles
-	mul $t1, $a0, 8192
-	add $t0, $t0, $t1   # &puzzles[i]
-	lw  $s0, 0($t0)     # puzzles[i]->head
-
-solve_loop:
-
-	lw	$a0, str($s0)
-	lw	$a1, str + 4($s0)
-	jal	puzzle_solve
-	sw	$v0, solution($s0)
-	lw	$s0, next($s0)       # puzzles[i]->next
-	bne	$s0, 0, solve_loop
-
-	sw	$t0, SOLVE_REQUEST
-
-	lw  $ra, 0($sp)
-	add $sp, $sp, 4
-	jr  $ra
-
 # main ############################################################
 #
 # arguments: none
@@ -316,23 +63,19 @@ main:
 	or	$t4, $t4, 1		           # global interrupt enable
 	mtc0	 $t4, $12		       # set interrupt mask (Status register)
 
-	# all free
-
 	# free up some registers
-	sub $sp, $sp, 16
+	sub $sp, $sp, __?
 	sw  $s0, 0($sp)
 	sw  $s1, 4($sp)
 	sw  $s2, 8($sp)
 	sw  $s3, 12($sp)
-
-	# all free
 
 main_loop:
 
 	li  $s0, 0         # i
 	li  $s1, -1        # j
 
-main_delv_check:                           # chcek if any puzzles have been delivered
+main_delv_check:
 
 	bge $s0, 5, main_delv_success_check
 
@@ -347,13 +90,12 @@ main_delv_check_inc:
 	add $s0, $s0, 1 # i++
 	j   main_delv_check
 
-main_delv_success_check:          
+main_delv_success_check:
 
 	blt $s1, 0, main_find_planet
-	move $a0, $s1
-	jal  move_to_planet                  # move to the planet 
-	move $a0, $s1
-	jal solve_puzzles                    # solve delivered puzzles
+	# request planet info
+	# move_to_planet(j)
+	# solve_puzzles(j)
 
 main_find_planet:
 
@@ -369,9 +111,8 @@ main_find_planet_loop:
 	lw  $t0, 0($t0)                        # pending_requests[i]
 	bne $t0, 0, main_find_planet_loop_inc  # pending_requests[i] != 0
 
-	move $a0, $s0
-	jal  move_to_planet
-	# request puzzle from planet i         # TODO
+	# move_to_planet(i)
+	# request planet from planet i
 
 main_find_planet_loop_inc:
 
